a6ceca434ca1675c1705ad6935e965aa85a4ea8f
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-08-25 15:15:07 -0300

    OrderItem, many-to-many association with extra attributes parte 2

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,24 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
+
+
+
+
+
+
+/*
+ * JPA Repository do spring-data-JPA que é um sub-framework do ecossitema spring, 
+ * é a camada mais em baixo da arquitetura;
+ * o UserRepository é o repository responsável por fazer operações com entidade User;
+ * pra a gente criar um UserRepository reutilizando o JPA-Repository basta  ele extender JpaRepository<User, Long>;
+ * JPARepository é uma interface;
+ * Não precisa criar a implementações da interface pq o spring-data-JPA ele já tem uma implementação padrão;
+ * @Repository não é necessário pq a interface já está herdando do JpaRepository, logo ele já está registrado como componente do spring (opcional);  
+ */
