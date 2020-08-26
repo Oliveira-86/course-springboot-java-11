06c02ef977d22bf93c8bf78b08b2fd16826eecea
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-08-25 22:33:49 -0300

    User Insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -30,6 +34,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
 
 
@@ -42,6 +53,10 @@ public class UserResource {
  * @GetMapping --> metodo que é um end point para acessar os usuários;
  * @Autowride --> Necessário cria um dependência para o Service;
  * @GetMapping(value = "/{id}") --> passar o valor da url e que a requisição seja aceita;
- * @PathVariable --> Annotation, serve para que o spring aceite o parametro(id) e considera-lo para chegar ate url "/{id}"	
+ * @PathVariable --> Annotation, serve para que o spring aceite o parametro(id) e considera-lo para chegar ate url "/{id}";
+ * 
+ * @RequestBody --> Annotation Para que o json seja deserializado para objeto na hora da requisição;
+ * 
+ * URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri() --> Forma padrão do Spring Boot de gerar um endereço(location), é esperado a resposta "201 Created" no padrão HTTP. 	
  */
 
